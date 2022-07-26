08d6ee5cc0798c0309eb7010580dea72df200cc0
Andrei <as@emit.digital>
2022-07-26 14:01:38 +0300

    metadata semantics

--- Resources/public/h5p/symfony-h5p.js
+++ Resources/public/h5p/symfony-h5p.js
@@ -10,6 +10,7 @@
             ns.apiVersion = H5PIntegration.editor.apiVersion;
             // Semantics describing what copyright information can be stored for media.
             ns.copyrightSemantics = H5PIntegration.editor.copyrightSemantics;
+            ns.metadataSemantics = H5PIntegration.editor.metadataSemantics;
             // Required styles and scripts for the editor
             ns.assets = H5PIntegration.editor.assets;
             // Required for assets
@@ -21,59 +22,26 @@
 
             var h5peditor;
             var $editor = $('#h5p-editor');
-            var $parameters = $('#h5p_parameters');
-            var $library = $('#h5p_library');
+            //change if bug name of your entity form here it H5_P
+            var $parameters = $('#h5_p_parameters');
+            var $library = $('#h5_p_library');
             var library = $library.val();
 
             if (h5peditor === undefined) {
                 h5peditor = new ns.Editor(library, $parameters.val(), $editor[0]);
             }
 
-            /*
-            let formIsUpdated = false;
-            const $form = $('form[name="h5p"]').submit(function (event) {
-              if ($type.length && $type.filter(':checked').val() === 'upload') {
-                return; // Old file upload
-              }
+            //modify with name of your entity in demo is H5_P why ? idk
+            $("form[name='h5_p']").submit(function () {
+                if (h5peditor !== undefined) {
+                    var editorParameters = h5peditor.getParams();
 
-              if (h5peditor !== undefined && !formIsUpdated) {
-
-                // Get content from editor
-                h5peditor.getContent(function (content) {
-
-                  // Set main library
-                  $library.val(content.library);
-
-                  // Set params
-                  $params.val(content.params);
-
-                  // Submit form data
-                  formIsUpdated = true;
-                  $form.submit();
-                });
-
-                // Stop default submit
-                event.preventDefault();
-              }
-            });
-            */
-
-            let formIsUpdated = false;
-            const $form = $("form[name='h5p']").submit(function () {
-
-                if (h5peditor !== undefined && !formIsUpdated) {
-                    // Get content from editor
-                    h5peditor.getContent(function (content) {
-                      // Set main library
-                      $library.val(content.library);
-                      // Set params
-                      $params.val(content.params);
-                      // Submit form data
-                      formIsUpdated = true;
-                      $form.submit();
-                    });
-                    // Stop default submit
-                    event.preventDefault();
+                    if (editorParameters !== undefined) {
+                        $library.val(h5peditor.getLibrary());
+                        $parameters.val(JSON.stringify(editorParameters));
+                    } else {
+                        return false;
+                    }
                 }
             });
         }
